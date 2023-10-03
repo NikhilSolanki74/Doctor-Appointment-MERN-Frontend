86564f98e1a34d537034c3168282439b9da8b509
Nikhil <solankinikhil74@gmail.com>
2023-10-03 15:48:07 +0530

    created register and login handler

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import {Form , Input} from 'antd'
+import {Form , Input , message} from 'antd'
 import "../styles/RegisterStyles.css"
-
+import { Link ,useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
-  const onFiniHandler = (values)=>{
-    console.log(values)
+ const navigate = useNavigate()
+  const onFiniHandler = async (values)=>{
+    try{
+          const res = await axios.post('api/v1/login' , values );
+            if(res.data.success){
+              localStorage.setItem("token" , res.data.token);
+              message.success("Login to HomePage successfully")
+              navigate('/')
+            }
+            else{
+              message.error(res.data.message)
+            }
+    }catch(error){
+       console.log(error)
+       message.error('something went wrong in login')
+    }
   }
   return (
     <>
@@ -15,9 +30,10 @@ const Login = () => {
                 <Form.Item label="Email" name="email">
                     <Input type="email" required />
                 </Form.Item>
-                <Form.Item label="Password" name="Password">
+                <Form.Item label="Password" name="password">
                     <Input type="password" required />
                 </Form.Item>
+                <Link to='/login' className='text-secondary mb-2'>Not A User Register Here </Link>
                 <button className='btn btn-success' type='submit'>Login</button>
             </Form>
         </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Form,Input } from 'antd'
+import { Form,Input,message } from 'antd'
 import "../styles/RegisterStyles.css"
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate } from 'react-router-dom'
+import axios from 'axios' // browser or nodejs which support ES6
+// const axios = require('axios') for common nodejs
 const Register = () => {
-  const onFinishHandler = (values)=>{
-    console.log(values)
+  const navigate = useNavigate();
+
+
+  const onFinishHandler =async (values)=>{
+     try{
+      const res = await axios.post("/api/v1/register" , values);
+      if(res.data.success){
+        message.success("Register Successfully !");
+        navigate("/login");
+      }else{
+        message.error(res.data.message);
+      }
+     }catch(error){
+         console.log(error);
+         message.error("something went wrong bro")
+     } 
   }
   return (
     <>
@@ -17,7 +33,7 @@ const Register = () => {
                 <Form.Item label="Email" name="email">
                     <Input type="email" required />
                 </Form.Item>
-                <Form.Item label="Password" name="Password">
+                <Form.Item label="Password" name="password">
                     <Input type="password" required />
                 </Form.Item>
                 <Link to='/login' className='text-secondary mb-2'>ALREADY A USER </Link>
