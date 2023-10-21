6782d79107fc70b990eda529ccadcfcb2d79389d
Nikhil <solankinikhil74@gmail.com>
2023-10-21 11:51:01 +0530

    created doctor and user routes and sample pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import PublicRoute from './components/PublicRoute'
 import ProtectedRoute from './components/ProtectedRoute'
 import ApplyDoctor from './pages/ApplyDoctor';
 import NotificationPage from './components/NotificationPage';
+import Users from './pages/admin/Users';
+import Doctors from './pages/admin/Doctors';
 function App() {
   const {loading} = useSelector(state=>state.alerts)
   return (
@@ -23,6 +25,16 @@ function App() {
           <Route path='/apply-doctor' element= {
           <ProtectedRoute>
             <ApplyDoctor/>
+          </ProtectedRoute>   
+          } />
+          <Route path='/admin/users' element= {
+          <ProtectedRoute>
+            <Users/>
+          </ProtectedRoute>   
+          } />
+          <Route path='/admin/Doctors' element= {
+          <ProtectedRoute>
+            <Doctors/>
           </ProtectedRoute>   
           } />
           <Route path='/notification' element= {
---
+++ src/pages/admin/Doctors.js
@@ -0,0 +1,15 @@
+import Layout  from './../../components/Layout'
+
+import React from 'react'
+
+const Doctors = () => {
+  return (
+    <Layout>
+        <h1>
+            all doctors
+        </h1>
+    </Layout>
+  )
+}
+
+export default Doctors
