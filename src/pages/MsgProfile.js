f4160e2a8a9132a6e59b8d78d67c5dc8f9845d0c
Nikhil <solankinikhil74@gmail.com>
2023-11-04 17:59:32 +0530

    added doctor name in msg page

--- src/pages/MsgProfile.js
+++ src/pages/MsgProfile.js
@@ -5,9 +5,12 @@ const MsgProfile = () => {
   return (
     <Layout>
       <h1 className='mx-3 bg-warning'>Sample Doctor Id For Authorization Testing </h1>
+      <h4 className="mx-5 bg-success text-white" >Name : Dr doctor doctor</h4>
+      
       <p className="mx-5" >Email : <p className="bg-success text-white">doctor@gmail.com</p></p>
-      <br/>
+      
       <p className="mx-5" >Password : <p className="bg-danger text-white">nikhil</p></p>
+
     </Layout>
   )
 }
